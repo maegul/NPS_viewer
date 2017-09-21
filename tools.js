0d745435dd295c48eb4a62011641b3da15bc5b84
Errol Lloyd <errolkjlloyd@gmail.com>
2017-09-21 19:29:45 +1000

    init beta finished

---
+++ tools.js
@@ -0,0 +1,20 @@
+var tools = [
+'python', 
+'R', // Keep this capitalised ... aids in filtering by tool
+'matlab',
+'latex',
+'tinkercad',
+'nltk',
+'kobo',
+'omeka',
+'slicer',
+'fusion',
+'hpc',
+'d3',
+'redcap',
+'autodesk',
+'scraping',
+'map',
+'plot.ly',
+'git'
+]
